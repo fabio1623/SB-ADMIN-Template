68afdf8c074313331e14ef35068057b8861e8213
fabio1623 <sarmentopedrofabio@gmail.com>
2017-07-29 12:33:12 +0200

    1st commit

---
+++ Bootstrap4Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bootstrap4Application.Startup))]
+namespace Bootstrap4Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
